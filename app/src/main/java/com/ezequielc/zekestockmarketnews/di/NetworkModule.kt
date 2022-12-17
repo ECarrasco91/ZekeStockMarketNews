47ebec5baafc6aa8142585074a3b69262b07dfa7
ECarrasco91 <ezequielcarrasco91@yahoo.com>
2022-12-16 20:05:01 -0500

    dependency injection for network

---
+++ app/src/main/java/com/ezequielc/zekestockmarketnews/di/NetworkModule.kt
@@ -0,0 +1,30 @@
+package com.ezequielc.zekestockmarketnews.di
+
+import com.ezequielc.zekestockmarketnews.network.StockDataService
+import com.ezequielc.zekestockmarketnews.util.STOCK_DATA_BASE_URL
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi() = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    @Provides
+    @Singleton
+    fun provideStockDataService(moshi: Moshi) = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(STOCK_DATA_BASE_URL)
+        .build()
+        .create(StockDataService::class.java)
+}
