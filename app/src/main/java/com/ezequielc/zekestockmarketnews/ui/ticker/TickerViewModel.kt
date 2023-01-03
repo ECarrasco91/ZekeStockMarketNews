bf2dfb5b0e9810b083befc006465473868044958
ECarrasco91 <ezequielcarrasco91@yahoo.com>
2023-01-02 23:53:58 -0500

    created ticker fragment

---
+++ app/src/main/java/com/ezequielc/zekestockmarketnews/ui/ticker/TickerViewModel.kt
@@ -0,0 +1,9 @@
+package com.ezequielc.zekestockmarketnews.ui.ticker
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class TickerViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
